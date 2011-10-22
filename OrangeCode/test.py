20f8dcd857a9d6fcaad0393014efc06d702c2114
Charles McCarthy <charlesmccarthyirl@gmail.com>
2011-10-22 21:54:55 +0100

    Compartmentalized a bit. Added things like StoppingCriteria, SelectionStrategy, etc.

--- OrangeCode/test.py
+++ OrangeCode/test.py
@@ -12,13 +12,30 @@ class Selection:
         self.selection = selection
         self.index = index
         
-    def delete_from(self, list):
+    def delete_from(self, example_table):
         index = self.index
         if index is None:
             index = orange_extensions.index(self.selection)
         
-        del(list[index])
-            
+        del(example_table[index])
+
+class StoppingCriteria:
+    def is_criteria_met(self, case_base, unlabelled_set):
+        pass
+
+class BudgetBasedStoppingCriteria(StoppingCriteria):
+    def __init__(self, budget, initial_case_base_size=0):
+        '''
+        
+        @param budget: The number of cases that can be labelled before the criteria is met.
+        @param initial_case_base_size: The size of the initial case base (so that a calculation 
+            can be done to see if it's expended
+        '''
+        self._budget = budget
+        self._initial_case_base_size = initial_case_base_size
+    
+    def is_criteria_met(self, case_base, unlabelled_set):
+        return len(case_base) - self._initial_case_base_size == self._budget
 
 class SelectionStrategy:
     def __init__(self, *args, **kwargs):
@@ -45,42 +62,33 @@ class RandomSelectionStrategy(SelectionStrategy):
         Given a collection of items to select from, selects an item and returns it (along with its index).
         @param collection: The indexable collection to select from.
         '''
-        
-
-
+        upTo = len(collection) - 1
+        r = self._random.randint(0, upTo)
+        return Selection(collection[r], r)
 
 def main():
-    data = orange.ExampleTable(r"C:\Users\Charles Mc\Dropbox\College\4th Year\FYP\Source\cmmc1_fyp\iris.arff")
-    rndind = orange.MakeRandomIndices2(data, p0=0.5)
+    data = orange.ExampleTable(r"iris.arff")
+    rndind = orange.MakeRandomIndices2(
+                data, 
+                p0=0.5)
     
     unlabelled = data.select(rndind, 0)
     test = data.select(rndind, 1)
     
     case_base = orange.ExampleTable(unlabelled.domain)
 
-    random.seed()
+    oracle = Oracle(orange.Example.get_class)
+    stopping_condition = BudgetBasedStoppingCriteria(10)
+    selection_strategy = RandomSelectionStrategy()
+    
+    points = [] # (x,y) elements
 
-    def select(exampleTable):
-        upTo = len(exampleTable) - 1
-        r = random.randint(0, upTo)
-        return r
-    
-    remaining = 10
-    def is_expended():
-        return remaining <= 0
-    
-    def classify(example):
-        return example.get_class()
-    
-    xs = []
-    ys = []
-    
-    while not is_expended():
-        selectedIndex = select(unlabelled)
+    while not stopping_condition.is_criteria_met(case_base, unlabelled):
+        selection = selection_strategy.select(unlabelled)
         
-        selectedExample = orange.Example(unlabelled[selectedIndex])
-        del(unlabelled[selectedIndex])
-        selectedExample.set_class(classify(selectedExample))
+        selectedExample = orange.Example(selection.selection)
+        selection.delete_from(unlabelled)
+        selectedExample.set_class(oracle.classify(selectedExample))
         
         case_base.append(selectedExample)
         
@@ -92,13 +100,11 @@ def main():
         number_in_case_base = len(case_base)
         classification_accuracy = CAs[0]
         
-        xs.append(number_in_case_base)
-        ys.append(classification_accuracy)
-        
-        print("Accuracy for %3d = %f" % (number_in_case_base ,classification_accuracy))
-        
-        remaining -= 1
+        points.append((number_in_case_base, classification_accuracy))
+        print("Accuracy for %3d = %f" % (number_in_case_base, classification_accuracy))
     
+    xs = [number_in_case_base for (number_in_case_base, classification_accuracy) in points]
+    ys = [classification_accuracy for (number_in_case_base, classification_accuracy) in points]
     plt.plot(xs, ys)
     plt.savefig("test.png")
     
