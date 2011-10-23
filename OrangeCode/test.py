91a4f03327107856e8dd2b82fce9af33c41d6ad9
Charles McCarthy <charlesmccarthyirl@gmail.com>
2011-10-23 11:35:07 +0100

    Moved loop that performs active learning to a SelectionStrategyEvaluator class

--- OrangeCode/test.py
+++ OrangeCode/test.py
@@ -3,6 +3,14 @@ import random
 import matplotlib.pyplot as plt
 import orange_extensions
 
+# TODO: Put in assert isinstance around the place for pydev.
+
+class Result:
+    def __init__(self, case_base_size, classification_accuracy, area_under_roc_curve):
+        self.case_base_size = case_base_size
+        self.classification_accuracy = classification_accuracy
+        self.area_under_roc_curve = area_under_roc_curve
+
 class Oracle:
     def __init__(self, classifyLambda):
         self.classify = classifyLambda
@@ -66,45 +74,66 @@ class RandomSelectionStrategy(SelectionStrategy):
         r = self._random.randint(0, upTo)
         return Selection(collection[r], r)
 
+class SelectionStrategyEvaluator:
+    def __init__(self, 
+                 oracle_generator, 
+                 stopping_condition_generator, 
+                 selection_strategy_generator,
+                 classifier_generator,
+                 **kwargs):
+        self.oracle_generator = oracle_generator
+        self.stopping_condition_generator = stopping_condition_generator
+        self.selection_strategy_generator = selection_strategy_generator
+        self.classifier_generator = classifier_generator
+    
+    def generate_results(self, test_set, unlabelled_set):
+        results = [] 
+        
+        # Order of assignment here important so that **locals has the right info (e.g. the stopping_criteria may care about the oracle)
+        case_base = orange.ExampleTable(unlabelled_set.domain)
+        selection_strategy = self.selection_strategy_generator(**locals())
+        oracle = self.oracle_generator(**locals())
+        stopping_condition = self.stopping_condition_generator(**locals())
+        
+        while not stopping_condition.is_criteria_met(case_base, unlabelled_set):
+            selection = selection_strategy.select(unlabelled_set) #TODO: Change to being a multi-selection thing
+            
+            selectedExample = orange.Example(selection.selection)
+            selection.delete_from(unlabelled_set)
+            selectedExample.set_class(oracle.classify(selectedExample))
+            
+            case_base.append(selectedExample)
+            
+            classifier = self.classifier_generator(case_base)
+            
+            testResults = orngTest.testOnData([classifier], test_set)
+            
+            case_base_size = len(case_base)
+            classification_accuracy = orngStat.CA(testResults)[0]
+            area_under_roc_curve = orngStat.AUC(testResults)[0]
+            
+            results.append(Result(case_base_size, classification_accuracy, area_under_roc_curve))
+            
+        return results
+
 def main():
+    oracle_generator = lambda *args, **kwargs: Oracle(orange.Example.get_class)
+    stopping_condition_generator = lambda *args, **kwargs: BudgetBasedStoppingCriteria(10)
+    selection_strategy_generator = lambda *args, **kwargs: RandomSelectionStrategy()
+    classifier_generator = lambda training_data, *args, **kwargs: orange.kNNLearner(training_data, k=5)
+    
     data = orange.ExampleTable(r"iris.arff")
-    rndind = orange.MakeRandomIndices2(
-                data, 
-                p0=0.5)
+    rndind = orange.MakeRandomIndices2(data, p0=.8)
     
-    unlabelled = data.select(rndind, 0)
-    test = data.select(rndind, 1)
+    test = data.select(rndind, 0)
+    unlabelled = data.select(rndind, 0, negate=1)
     
-    case_base = orange.ExampleTable(unlabelled.domain)
-
-    oracle = Oracle(orange.Example.get_class)
-    stopping_condition = BudgetBasedStoppingCriteria(10)
-    selection_strategy = RandomSelectionStrategy()
+    evaluator = SelectionStrategyEvaluator(**locals())
+    points = evaluator.generate_results(test, unlabelled)
+    #print("Accuracy for %3d = %f" % (case_base_size, classification_accuracy))
     
-    points = [] # (x,y) elements
-
-    while not stopping_condition.is_criteria_met(case_base, unlabelled):
-        selection = selection_strategy.select(unlabelled)
-        
-        selectedExample = orange.Example(selection.selection)
-        selection.delete_from(unlabelled)
-        selectedExample.set_class(oracle.classify(selectedExample))
-        
-        case_base.append(selectedExample)
-        
-        classifier = orange.kNNLearner(case_base, k=1)
-        
-        testResults = orngTest.testOnData([classifier], test)
-        CAs = orngStat.CA(testResults)
-        
-        number_in_case_base = len(case_base)
-        classification_accuracy = CAs[0]
-        
-        points.append((number_in_case_base, classification_accuracy))
-        print("Accuracy for %3d = %f" % (number_in_case_base, classification_accuracy))
-    
-    xs = [number_in_case_base for (number_in_case_base, classification_accuracy) in points]
-    ys = [classification_accuracy for (number_in_case_base, classification_accuracy) in points]
+    xs = [result.case_base_size for result in points]
+    ys = [result.classification_accuracy for result in points]
     plt.plot(xs, ys)
     plt.savefig("test.png")
     
