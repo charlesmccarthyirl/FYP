fda1093b2f3fbe5c8a779e40c65ecb36bea84563
Charles McCarthy <charles@ubuntu.(none)>
2012-01-16 17:41:37 +0000

    Changed data sets to reference the .gz versions

--- OrangeCode/DataSets.py
+++ OrangeCode/DataSets.py
@@ -1,16 +1,16 @@
 from BreadAndButter import create_named_data_set_generators
 
 named_data_sets = create_named_data_set_generators((
-    ('iris.pcdb', {'sc': 20}),
-    ('dermatology.pcdb', {'sc': 40}),
-    ('wine.pcdb', {'sc': 25}),
-    ('zoo.pcdb', {'sc': 25}),
-    ('hepatitis.pcdb', {'sc': 10}),
-    ('breathalyzer.pcdb', {'sc': 50}),
-    ('haberman.pcdb', {'sc': 40}),
-    ('glass.pcdb', {'sc': 40}),
-    ('tae.pcdb', {'sc': 30}),
-    ('heart_disease.pcdb', {'sc': 30}),
-    ('lung-cancer.pcdb', {'sc': 10}),
-    ('pima-indians-diabetes.pcdb', {'sc': 60}),
+    ('iris.pcdb.gz', {'sc': 20}),
+    ('dermatology.pcdb.gz', {'sc': 40}),
+    ('wine.pcdb.gz', {'sc': 25}),
+    ('zoo.pcdb.gz', {'sc': 25}),
+    ('hepatitis.pcdb.gz', {'sc': 10}),
+    ('breathalyzer.pcdb.gz', {'sc': 50}),
+    ('haberman.pcdb.gz', {'sc': 40}),
+    ('glass.pcdb.gz', {'sc': 40}),
+    ('tae.pcdb.gz', {'sc': 30}),
+    ('heart_disease.pcdb.gz', {'sc': 30}),
+    ('lung-cancer.pcdb.gz', {'sc': 10}),
+    ('pima-indians-diabetes.pcdb.gz', {'sc': 60}),
 ))
