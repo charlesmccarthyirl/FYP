e5344cfc5a131e02a83ef9098d5d1a6e7489ea20
Charles McCarthy <charles@ubuntu.(none)>
2012-01-17 21:50:03 +0000

    Changed names of data files to be more friendly for use in latex. Removed pima-indians due to size.

--- OrangeCode/DataSets.py
+++ OrangeCode/DataSets.py
@@ -10,7 +10,6 @@ named_data_sets = create_named_data_set_generators((
     ('haberman.pcdb.gz', {'sc': 40}),
     ('glass.pcdb.gz', {'sc': 40}),
     ('tae.pcdb.gz', {'sc': 30}),
-    ('heart_disease.pcdb.gz', {'sc': 30}),
-    ('lung-cancer.pcdb.gz', {'sc': 10}),
-    ('pima-indians-diabetes.pcdb.gz', {'sc': 60}),
+    ('heartdisease.pcdb.gz', {'sc': 30}),
+    ('lungcancer.pcdb.gz', {'sc': 10}),
 ))
