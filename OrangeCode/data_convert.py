f844f38c1fe71c89616a0c31bb67c10567b05177
Charles McCarthy <charlesmccarthyirl@gmail.com>
2012-01-20 23:55:56 +0000

    Changed normal data file script to output .pcdb.gz's instead of just .pcdb's

--- OrangeCode/data_convert.py
+++ OrangeCode/data_convert.py
@@ -20,6 +20,6 @@ for df in data_files:
     base_name = os.path.splitext(os.path.basename(df))[0]
     
     input_file = os.path.join(data_dir, df)
-    output_file = os.path.join(data_dir, base_name + ".pcdb")
+    output_file = os.path.join(data_dir, base_name + ".pcdb.gz")
     
-    main(None, input_file, output_file, 'Euclidean', 'proto')
+    main(None, input_file, output_file, 'Euclidean', 'proto', 1)
