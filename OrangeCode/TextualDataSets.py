29efd179633dbf1748fb7922384f459c4d8fb453
Charles McCarthy <charlesmccarthyirl@gmail.com>
2012-01-22 20:23:19 +0000

    Added textual datasets

---
+++ OrangeCode/TextualDataSets.py
@@ -0,0 +1,8 @@
+from BreadAndButter import create_named_data_set_generators
+
+named_data_sets = create_named_data_set_generators((
+    ('WinXwin.pcdb.gz', {'sc': 150}),
+    ('Comp.pcdb.gz', {'sc': 150}),
+    ('Talk.pcdb.gz', {'sc': 150}),
+    ('Vehicle.pcdb.gz', {'sc': 150}),
+))
