7384322230430af5da6771ce9a84d2aaeb7fd15a
Charles McCarthy <charlesmccarthyirl@gmail.com>
2012-02-10 23:51:59 +0000

    Bugfix to put in correct directory.

--- OrangeCode/builder.py
+++ OrangeCode/builder.py
@@ -9,10 +9,15 @@ from subprocess import call
 from glob import glob
 import shutil
 import logging
+from pprint import pprint
 
 def maybe_make_dirs(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+def my_call(args):
+    pprint(" ".join(args))
+    call(args)
 
 STORAGE_DIR = "~/FYP/data_dir/"
 REPORT_DIR = "~/FYP/experiment_outputs/"
@@ -21,7 +26,7 @@ non_textual_dir_name = "non_textual"
 textual_dir_name = "textual"
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
     
     STORAGE_DIR = os.path.expanduser(STORAGE_DIR)
     REPORT_DIR = os.path.expanduser(REPORT_DIR)
@@ -38,27 +43,28 @@ if __name__ == '__main__':
         d = os.path.join(STORAGE_DIR, cat_name)
         maybe_make_dirs(d)
         logging.info("Beginning experiment execution on %s" % dsfn)
-        call(["python", "-O", "test.py", "experiment1", dsfn, d, "0"], shell=True)
-        for fn in glob(os.path.join(STORAGE_DIR, "*.pdf")):
+        my_call(["python", "-O", "test.py", "experiment1", dsfn, d, "0"])
+        for fn in glob(os.path.join(d, "*.pdf")):
             shutil.copyfile(fn, os.path.join(REPORT_DIR, os.path.basename(fn)))
-        shutil.copyfile(os.path.join(STORAGE_DIR, "summary.csv"),
+        shutil.copyfile(os.path.join(d, "summary.csv"),
                         os.path.join(REPORT_DIR, cat_name + "_summary.csv"))
         logging.info("Generating data stats")
-        call("python", "-O", "gen_data_stats.py", dsfn,
-             os.path.join(REPORT_DIR, cat_name + "_data_stats.csv"), shell=True)
+        args = ["python", "-O", "gen_data_stats.py", dsfn,
+             os.path.join(REPORT_DIR, cat_name + "_data_stats.csv")]
+        my_call(args)
     
     dsn = 'WinXwin'
     timings_fn = os.path.join(STORAGE_DIR, dsn + ".csv")
     if not os.path.exists(timings_fn):
         logging.info("Generating timings for %s" % dsn)
-        call(["python", "-O", "CaseProfilingTimings.py", dsn,
-             timings_fn], shell=True)
+        my_call(["python", "-O", "CaseProfilingTimings.py", dsn,
+             timings_fn])
     
     timings_pdf_fn = os.path.join(REPORT_DIR, dsn + "_timings_incr_only.pdf")
     
     logging.info("Plotting timings")
-    call(["python", "-O", "plotter.py", timings_fn, timings_pdf_fn, 
-         "-c", "1",  "--y_title",  "Time to build RCDL profiles (/s)", "--name", dsn], shell=True)
+    my_call(["python", "-O", "plotter.py", timings_fn, timings_pdf_fn, 
+         "-c", "1",  "--y_title",  "Time to build RCDL profiles (/s)", "--name", dsn])
     
     logging.info("Generating selection graphs")
     dsn = 'zoo'
@@ -71,7 +77,7 @@ if __name__ == '__main__':
         out_file = os.path.join(ssdir, vn + ".pdf")
         if not os.path.exists(out_file):
             logging.info("Generating selection graph for variation %s" % vn)
-            call(["python", "-O", "gen_selection_graphs.py", dsn, fn, out_file], shell=True) 
+            my_call(["python", "-O", "gen_selection_graphs.py", dsn, fn, out_file]) 
         shutil.copyfile(out_file,
                         os.path.join(rsdir, os.path.basename(out_file)))
         
