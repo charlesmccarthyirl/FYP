db8989673a880f6fbae1c0e187db352c0772678c
Charles McCarthy <charlesmccarthyirl@gmail.com>
2012-03-26 17:44:30 +0100

    Updated bad hosts.

--- OrangeCode/Hosts/college.py
+++ OrangeCode/Hosts/college.py
@@ -3,7 +3,10 @@ Created on Mar 21, 2012
 
 @author: charles
 '''
-bad_hosts = ["cs1-17-06","cs1-17-11","cs1-17-19", "csg24-32", "cs1-31-16"]
+bad_hosts = ["cs1-17-06","cs1-17-11","cs1-17-19", "csg24-32", "cs1-31-16", "csg22-06", "csg22-20", "csg26-02"]
+bad_hosts += ["mmlab%02d" % i for i in range(3, 26)] 
+bad_hosts += ["cs1-31-%02d" % i for i in range(2, 16)]
+
 hosts = ["csg21-%02d" % i for i in range(2, 48)] #g21
 hosts += ["cs1-17-%02d" % i for i in range(3, 22)] #1-17 
 hosts += ["csg24-%02d" % i for i in range(2, 48)] + ["cs1-09-%02d" % i for i in range(1, 25)] + ["csg20-26", "csg20-37", "cs1-31-16"] #g24
