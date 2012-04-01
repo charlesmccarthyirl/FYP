aa5cd412d0da515f33a2c12361a6cd2f4f9fe9da
Charles McCarthy <charlesmccarthyirl@gmail.com>
2012-04-01 13:16:06 +0100

    Removed Margin Sampling from Baselines

--- OrangeCode/Experiments/baselines.py
+++ OrangeCode/Experiments/baselines.py
@@ -1,7 +1,7 @@
 from all_experiments import experiment as all_experiment
 from utils import sub_pairs
 
-names = ["Random Selection", "Uncertainty Sampling", "Margin Sampling", "Maximum Diversity Sampling",
+names = ["Random Selection", "Uncertainty Sampling", "Maximum Diversity Sampling",
          "Sparsity Minimization", "Diversity + Density Maximization", "Certainty + Sparsity Minimization"]
 
 experiment = all_experiment.create_sub_experiment(names)
